5ab3b9d1ccc3b8af448b20c5532c9071f167cf4e
adil <adilforms@gmail.com>
2017-03-11 00:50:23 -0500

    my first

---
+++ project/src/main/java/dummypro/App.java
@@ -0,0 +1,13 @@
+package dummypro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
